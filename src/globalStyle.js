58b10a9093fa4cf5474b5743ea86f454f6725e23
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-09-14 13:58:10 -0300

    Feat: new folders and archives

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,12 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 const globalStyle = createGlobalStyle `
-    * {
+@import url("https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;1,400&display=swap");    
+
+* {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: 'Roboto', sans-serif;
+        font-family: 'Nunito', sans-serif;
         outline: none;
+        background: #ECEEF2;
     }
 `
 
