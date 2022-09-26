cb60fd3bbe8be539c5a820ddbf2666a38d353290
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-09-26 14:24:34 -0300

    Style: basic style for input component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Checkbox } from './components/micro/checkbox/Checkbox'
 import { Title } from './components/micro/title/Title'
 import { Buttons } from './components/micro/buttons/Buttons'
+import { Inputs } from './components/micro/inputs/Inputs'
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Checkbox />
       <Title idTitle="title" title="Intern Sign Up" />
       <Buttons classButton="smallButton" textButton="Register" />
+      <Inputs labelText="Oi" labelFor="Oi" inputType="text" inputId="Oi" inputClass="bigInput" placehInput="Teste1" />
+      <Inputs labelText="Tchau" labelFor="Tchau" inputType="date" inputId="Tchau" inputClass="smallInput" placehInput="Teste2" />
+      <Inputs labelText="Oi *" labelFor="OiDnv" inputType="password" inputId="OiDnv" inputClass="mediumInput" placehInput="Teste3" />
     </div>
   );
 }
