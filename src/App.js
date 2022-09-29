8206355e4a30c357cb0f4a595521399f48345d43
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-09-29 16:04:22 -0300

    Feat: success page without logics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Home } from './components/pages/home/Home'
+import { Success } from './components/pages/success/Success'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Success />
     </div>
   );
 }
