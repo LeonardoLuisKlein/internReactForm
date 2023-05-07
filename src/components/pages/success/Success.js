9375c2fcb5d62f1de03800aa2916322e53727f0c
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-05-07 20:17:59 -0300

    Feat: adding routes logics

--- src/components/pages/success/Success.js
+++ src/components/pages/success/Success.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 import './Success.scss'
 
@@ -7,13 +8,27 @@ import { Buttons } from "../../micro/buttons/Buttons";
 import { Image } from "../../micro/image/Image";
 
 export const Success = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica de envio do formulário
+
+    // Após o envio bem-sucedido do formulário, navegar para a página de sucesso
+    navigate('/');
+  };
+
+
   return (
-    <section className='successSec'>
+    <form id='successSec' onSubmit={handleSubmit}>
+        <div id="title">
         <Title idTitle="basicTitle" title="Success!" />
-        <Image />
-        <div className='buttonCenter'>
-        <Buttons classButton="bigButton" textButton="Back to home page" />
+        <Image imageClass="titleImage"/>
         </div>
-    </section>
+        <div className='buttonCenter'>
+        <Buttons classButton="bigButton" textButton="Back to home page" type="submit"/>
+        </div>
+    </form>
   )
 }
