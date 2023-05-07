3c7cc65505e785196c5eab03e8e4fe87cbe482bb
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-05-07 20:27:37 -0300

    Feat: adding error 404 page

---
+++ src/components/pages/error404/Error404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Imagem from "../../../assets/error404.gif";
+
+export const Error404 = () => {
+  return (
+    <div className="errorPage">
+        <img src={Imagem} alt="Descrição da imagem" />
+    </div>
+  )
+}
